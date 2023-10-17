1fb2cc79016bc2f402d9f8c3e8e8d6054fb89701
Amara Khan <amaraskhan@gmail.com>
2023-10-17 19:55:52 -0400

    documentation: updated README with status of exercises

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ Companion repo to [The Coding Inteview Bootcamp: Algorithms + Data Structures](h
 4. maxchar - completed
 5. fizzbuzz - completed
 6. chunk - completed
+7. anagrams - completed
+8. capitalize
+9. steps
+10. pyramid
 etc...
--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+
+    aChars = charCount(stringA);
+    bChars = charCount(stringB);
+
+    if (Object.keys(aChars).length !== Object.keys(bChars).length) {
+        return false;
+    }
+
+    for (const key in aChars) {
+        if (bChars[key] !== aChars[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function charCount(str) {
+
+    charMap = {};
+    str.split('').forEach(char => {
+        if (char.match(/[a-z]/i)) {
+            charMap[char.toLowerCase()] = charMap[char.toLowerCase()] + 1 || 1;
+        }
+    });
+
+    return charMap;
+}
 
 module.exports = anagrams;
